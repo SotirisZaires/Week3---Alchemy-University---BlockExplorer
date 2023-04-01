853b14a4b9b53ff633e98f7c52f454c1a788f821
unknown <sotiriszair@gmail.com>
2023-04-01 13:19:19 +0300

    home page implemantion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { Alchemy, Network } from 'alchemy-sdk';
+import { Alchemy, Network, Utils } from 'alchemy-sdk';
 import { useEffect, useState } from 'react';
-
+import React from "react";
 import './App.css';
 
 // Refer to the README doc for more information about using API
@@ -21,16 +21,83 @@ const alchemy = new Alchemy(settings);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
+  const [latestBlocks, setLatestBlocks] = useState([]);
+  const [latestTxs, setLatestTxs] = useState([]);
 
   useEffect(() => {
     async function getBlockNumber() {
-      setBlockNumber(await alchemy.core.getBlockNumber());
+      let response = await alchemy.core.getBlockNumber();
+      setBlockNumber(response);
     }
-
+    async function getLatestBlocks() {
+        let blocksTemp = [];
+        //let blockNumber = await alchemy.core.getBlockNumber();
+        for (let i = blockNumber; i > blockNumber - 10; i--) {
+            const block = await alchemy.core.getBlock(i);
+            blocksTemp.push(block);
+          }
+        setLatestBlocks(blocksTemp);
+    }
+    async function getLatestTransactions() {
+       //let blockNumber = await alchemy.core.getBlockNumber();
+        const block = await alchemy.core.getBlockWithTransactions(blockNumber);
+        setLatestTxs(block.transactions.slice(0,10));
+    }
     getBlockNumber();
-  });
+    getLatestBlocks();
+    getLatestTransactions()
+  }, [blockNumber]);
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+  return(
+    <div className='app'>
+    <header className='App-header'>MyEtherScan</header>
+        <main>
+        <table className='container'>
+            <thead>
+                <tr>
+                    <th>Block</th>
+                    <th>Hash</th>
+                    <th>Miner</th>
+                    <th>Txs</th>
+                </tr>
+            </thead>
+            <tbody>
+                {latestBlocks.map((block) => (
+                <tr key={block.number}>
+                    <td className='numberB'>{block.number}</td>
+                    <td>{block.hash.slice(0,10)}...{block.hash.slice(-10)}</td>
+                    <td>{block.miner.slice(0,15)}...</td>
+                    <td>{block.transactions.length}</td>
+                </tr>
+                ))}
+            </tbody>
+        </table>
+        <table className='container'>
+            <thead> 
+                <tr>
+                    <th>Transaction</th>
+                    <th>From</th>
+                    <th>To</th>
+                    <th>Value</th>
+                </tr>
+            </thead>
+            <tbody>
+                {latestTxs.map((tx) => (
+                <tr key={tx.hash}>
+                    <td>{tx.hash.slice(0,20)}</td>
+                    <td>{tx.from.slice(0,10)}...{tx.from.slice(-10)}</td>
+                    <td>{tx.to.slice(0,10)}...{tx.to.slice(-10)}</td>
+                    <td className='ethval'>{Math.floor(Utils.formatEther(tx.value)*10000)/10000} ETH</td>
+                </tr>
+                ))}
+            </tbody>
+        </table>
+        </main>
+        <div className='diva'>
+
+        </div>
+    </div>
+  );
 }
 
 export default App;
